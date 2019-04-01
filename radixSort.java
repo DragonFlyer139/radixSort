d8e4e43c49f22aafd85b9f95854ed187d3dc877a
DragonFlyer139 <40473280+DragonFlyer139@users.noreply.github.com>
2019-03-31 19:37:34 -0500

    Update radixSort.java

--- radixSort.java
+++ radixSort.java
@@ -3,29 +3,41 @@ public class radixSort {
 	
 	public static void main(String[] args) 
 	{
-		int[] arr = {24234, 113234125, 46, 4234, 634, 12, 6543};
+		// initialize a new array
+		int n = 1000;
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = (int)(Math.random()*n);
+		
+		// find the exponent for the maximum value in the array
 		System.out.println(getMaxDigits(arr));
 		
+		// print out the sorted array
 		radix(arr);
 		for (int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
 	}
 	
+	// find the exponent for the maximum value in the array
 	public static int getMaxDigits(int[] arr)
 	{
+		// find the maximum value of the array 'arr'
 		int max = arr[0];
 		for (int i = 1; i < arr.length; i++)
 			if (arr[i] > max)
 				max = arr[i];
 		
+		// find the number of digits of 'max'
 		int count = 1;
 		while (Math.pow(10, count) < max)
 			count++;
 		return count;
 	}
 	
+	// radix sort the array
 	public static void radix(int[] arr)
 	{
+		// initialize all the linked list buckets
 		LinkedList b0 = new LinkedList();
 		LinkedList b1 = new LinkedList();
 		LinkedList b2 = new LinkedList();
@@ -36,28 +48,34 @@ public class radixSort {
 		LinkedList b7 = new LinkedList();
 		LinkedList b8 = new LinkedList();
 		LinkedList b9 = new LinkedList();
-		
-		LinkedList[] box = {b0, b1, b2, b3, b4, b5, b6, b7, b8, b9};
+		// place the linked lists b0-b9 into an array
+		LinkedList[] bin = {b0, b1, b2, b3, b4, b5, b6, b7, b8, b9};
 		
 		int m = 10;
 		int n = 1;
 		int exp = getMaxDigits(arr);
 		
+		// loop through every place value up to 'exp'
 		for (int e = 0; e < exp; e++)
 		{
 			for (int i = 0; i < arr.length; i++) 
 			{
-				int nbr = (arr[i] % m) / n;
-				box[nbr].insertRear(arr[i]);
+				// go through the array and find the value of the digit in place 'e'
+				int val = (arr[i] % m) / n;
+				// place the arr[i] into the corresponding bin for the round
+				bin[val].insertRear(arr[i]);
 			}
 			
 			int count = 0;
-			for (int j = 0; j < box.length; j++)
+			// loop through bins 'b0' - 'b9' within 'bin'
+			for (int j = 0; j < bin.length; j++)
 			{
-				while (!box[j].isEmpty())
-					arr[count++] = (int) box[j].removeFront();
+				// remove the values from the bin in order and place back into 'arr'
+				while (!bin[j].isEmpty())
+					arr[count++] = (int) bin[j].removeFront();
 			}
 			
+			// increment 'm' and 'n'
 			m *= 10;
 			n *= 10;
 		}	
